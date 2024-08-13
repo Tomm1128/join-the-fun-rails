d8b8ca06ddf68d065adbf2fdaa6300fcb0385625
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-13 11:08:03 -0400

    Create migrations

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration[5.0]
   def change
     create_table :rides do |t|
+      t.references :taxi
+      t.references :passenger
       t.timestamps null: false
     end
   end
